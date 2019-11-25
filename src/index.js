35082350a6a6af75b2495b8cb6d0050d08c66c34
Douglass Turner <douglass.turner@gmail.com>
2019-11-25 15:05:14 -0500

    update

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import FileLoadWidget from './fileLoadWidget.js';
 import MultipleFileLoadController from './multipleFileLoadController.js';
 import TrackLoadController, { trackLoadControllerConfigurator } from './trackLoadController.js';
 import Alert from "./igvjs/ui/alert.js";
+import oauth from "./igvjs/oauth.js";
+import igvxhr from "./igvjs/igvxhr.js";
 import * as Widgets from './utils.js';
-export { Alert, Widgets, FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, trackLoadControllerConfigurator }
+export { oauth, igvxhr, Alert, Widgets, FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, trackLoadControllerConfigurator }
