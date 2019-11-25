1100d49de40d4974a0e0cd5b6814e2cc3245bddb
Douglass Turner <douglass.turner@gmail.com>
2019-11-25 11:13:21 -0500

    export'ing

--- dist/igv-widgets.js
+++ dist/igv-widgets.js
@@ -9495,4 +9495,4 @@ const trackLoadMultipleFileLoadConfigurator = ({ browser, modal, localFileInput,
 
 };
 
-export { FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, utils as Widgets, trackLoadControllerConfigurator };
+export { Alert, FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, utils as Widgets, trackLoadControllerConfigurator };
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import FileLoadManager from './fileLoadManager.js';
 import FileLoadWidget from './fileLoadWidget.js';
 import MultipleFileLoadController from './multipleFileLoadController.js';
 import TrackLoadController, { trackLoadControllerConfigurator } from './trackLoadController.js';
+import Alert from "./igvjs/ui/alert.js";
 import * as Widgets from './utils.js';
-export { Widgets, FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, trackLoadControllerConfigurator }
+export { Alert, Widgets, FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, trackLoadControllerConfigurator }
