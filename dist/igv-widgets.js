8e266addc5bc57387fccbee040d981482c2eed6d
Jim Robinson <jrobinso@broadinstitute.org>
2020-11-02 15:29:22 -0800

    update data-modal version for Encode colors

--- dist/igv-widgets.js
+++ dist/igv-widgets.js
@@ -8872,6 +8872,29 @@ class ModalTable {
 
 }
 
+/**
+ * Return a color for the ChIP target, or undefined if no color is assigned.
+ * @param target
+ * @returns {string}
+ */
+function colorForTarget(target) {
+
+    const t = target.toLowerCase();
+    if (t.startsWith("h3k4")) {
+        return "rgb(0,150,0)"
+    } else if (t.startsWith("h3k27")) {
+        return "rgb(200,0,0)"
+    } else if (t.startsWith("h3k36")) {
+        return "rgb(0,0,150)"
+    } else if (t.startsWith("h3k9")) {
+        return "rgb(100,0,0)"
+    } else if (t === "ctcf") {
+        return "black"
+    } else {
+        return undefined;
+    }
+}
+
 const encodeTrackDatasourceOtherConfigurator = genomeId => {
 
     return {
@@ -8913,8 +8936,8 @@ const encodeTrackDatasourceOtherConfigurator = genomeId => {
             ],
         parser: undefined,
         selectionHandler: selectionList => {
-            return selectionList.map(({ name, HREF }) => {
-                return { name, url: HREF }
+            return selectionList.map(({ name, HREF, Target }) => {
+                return { name, url: HREF, color: colorForTarget(Target) }
             })
         }
 
@@ -8963,8 +8986,8 @@ const encodeTrackDatasourceSignalConfigurator = genomeId => {
             ],
         parser: undefined,
         selectionHandler: selectionList => {
-            return selectionList.map(({ name, HREF }) => {
-                return { name, url: HREF }
+            return selectionList.map(({ name, HREF, Target }) => {
+                return { name, url: HREF, color: colorForTarget(Target) }
             })
         }
 
